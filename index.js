377d7d0d28e211100b9341cdb5bc5a0f666862b6
ArmsAndArrows <121558109+ArmsAndArrows@users.noreply.github.com>
2023-04-08 15:57:43 +0600

    Change js file

--- index.js
+++ index.js
@@ -1,119 +1,122 @@
 "use strict";
 
-///CONTROLLERS
 const day = document.querySelector("#day");
 const month = document.querySelector("#month");
 const year = document.querySelector("#year");
 const button = document.querySelector("#button");
-
 const operands = [day, month, year];
 
-///ERRORMSG
-
-const dayError = document.getElementById("day-err");
-const monthError = document.getElementById("month-err");
-const yearError = document.getElementById("year-err");
-
-///DATE
+const dayError = document.querySelector("#day-err");
+const monthError = document.querySelector("#month-err");
+const yearError = document.querySelector("#year-err");
 
 const date = new Date();
-const [currentMonth, currentDay, currentYear] = [
-  date.getMonth() + 1,
-  date.getDate(),
-  date.getFullYear(),
-];
+const currentMonth = date.getMonth() + 1;
+const currentDay = date.getDate();
+const currentYear = date.getFullYear();
 const dateArray = [currentMonth, currentDay, currentYear];
 
-///INPUT FOR DIGITS ONLY
-
 function numInput(event) {
-  if (
-    event.key === "Backspace" ||
-    event.key === "Tab" ||
-    event.key === "ArrowLeft" ||
-    event.key === "ArrowRight"
-  ) {
-    return;
-  }
-
-  if (isNaN(parseInt(event.key))) {
-    event.preventDefault(); // prevent the key from being typed
+  const validKeys = ["Backspace", "Tab", "ArrowLeft", "ArrowRight"];
+  if (!validKeys.includes(event.key) && isNaN(parseInt(event.key))) {
+    event.preventDefault();
   }
 }
 
-operands.forEach((element) => {
-  element.addEventListener("keydown", numInput);
-  if (element.value == undefined) {
-    console.log("dssd");
-  }
-});
-
-////BUTTON LOGIC
-
-button.addEventListener("click", function () {
-  operands.forEach((element) => {
-    if (element.value == "") {
-      document.getElementById(`${element.id}-err`).innerHTML =
-        "This field is required";
-      element.style.borderColor = "red";
-      document.getElementById("result-year").innerHTML = "--";
-      document.getElementById("result-month").innerHTML = "--";
-      document.getElementById("result-day").innerHTML = "--";
-    } else {
-      document.getElementById(`${element.id}-err`).innerHTML = "";
-      element.style.borderColor = "rgb(107 114 128)";
-    }
+function clearErrors() {
+  dayError.innerHTML = "";
+  monthError.innerHTML = "";
+  yearError.innerHTML = "";
+  operands.forEach((operand) => {
+    operand.style.borderColor = "rgb(107, 114, 128)";
   });
-  if (year.value > currentYear) {
-    yearError.innerHTML = "Must be in past";
+}
+
+function validateInput() {
+  let isValid = true;
+  const inputDate = new Date(year.value, month.value - 1, day.value);
+
+  if (!year.value || !month.value || !day.value) {
+    dayError.innerHTML = "Please enter a valid date";
     year.style.borderColor = "red";
+    isValid = false;
+  } else if (inputDate > date) {
+    dayError.innerHTML = "Date must not be in the future";
+    day.style.borderColor = "red";
+    isValid = false;
   }
-  if (month.value > 12 || month.value === "0" || month.value === "00") {
+
+  if (year.value > currentYear || year.value < 1900) {
+    yearError.innerHTML = "Must be a valid year";
+    day.style.borderColor = "red";
+    isValid = false;
+  }
+
+  if (month.value > 12 || month.value < 1) {
     monthError.innerHTML = "Must be a valid month";
     month.style.borderColor = "red";
+    isValid = false;
   }
-  if (day.value > 31 || day.value === "0" || day.value === "00") {
+
+  if (day.value > 31 || day.value < 1) {
     dayError.innerHTML = "Must be a valid day";
     day.style.borderColor = "red";
-  }
-  if (year.value < 1900) {
-    yearError.innerHTML = "Must be a valid year";
-    year.style.borderColor = "red";
+    isValid = false;
   }
 
-  if (
-    year.value &&
-    month.value &&
-    day.value &&
-    parseInt(year.value) < currentYear &&
-    parseInt(month.value) <= 12 &&
-    parseInt(day.value) <= 31 &&
-    parseInt(year.value) > 1900
-  ) {
-    let countedYear = currentYear - parseInt(year.value);
-    let countedMonth = "";
-    let countedDay = "";
+  return isValid;
+}
 
-    if (parseInt(month.value) > currentMonth) {
-      countedYear -= 1;
-      countedMonth = 12 + currentMonth - parseInt(month.value);
-    } else {
-      countedMonth = currentMonth - parseInt(month.value);
-    }
-
-    if (parseInt(day.value) >= currentDay) {
-      if (countedMonth > parseInt(month.value)) {
-        countedMonth -= 1;
-        countedDay = parseInt(day.value) - currentDay;
-      } else {
-        countedDay = parseInt(day.value) - currentDay;
-      }
-    } else {
-      countedDay = 31 - parseInt(day.value);
-    }
-
-    document.getElementById("result-year").innerHTML = String(countedYear);
-    document.getElementById("result-month").innerHTML = String(countedMonth);
-    document.getElementById("result-day").innerHTML = String(countedDay);
+button.addEventListener("click", function () {
+  clearErrors();
+  if (validateInput()) {
+    const birthDate = new Date(year.value, month.value - 1, day.value);
+    const age = calculateAge(birthDate);
+    animateNumber(0, age.years, document.getElementById("result-year"));
+    animateNumber(0, age.months, document.getElementById("result-month"));
+    animateNumber(0, age.days, document.getElementById("result-day"));
+    document.getElementById("result-year").innerHTML = age.years;
+    document.getElementById("result-month").innerHTML = age.months;
+    document.getElementById("result-day").innerHTML = age.days;
   }
 });
+
+function calculateAge(birthDate) {
+  const today = new Date();
+  const diff = today - birthDate;
+  let years = today.getFullYear() - birthDate.getFullYear();
+  let months = today.getMonth() - birthDate.getMonth();
+  let days = today.getDate() - birthDate.getDate();
+
+  if (months < 0 || (months === 0 && days < 0)) {
+    years--;
+    months = 12 - Math.abs(months);
+    days = 31 - Math.abs(days);
+  } else if (months > 0 && days < 0) {
+    months--;
+    const monthDate = new Date(today.getFullYear(), today.getMonth() - 1, 0);
+    days = monthDate.getDate() - Math.abs(days);
+  }
+
+  return { years: years, months: months, days: days };
+}
+
+// Event listeners
+operands.forEach((operand) => {
+  operand.addEventListener("keydown", numInput);
+});
+
+///ANIMATION
+
+function animateNumber(startNum, targetNum, element) {
+  let currentNum = startNum;
+  const step = Math.ceil((targetNum - startNum) / 20); // divide the difference by 10 to get 10 steps
+  const interval = setInterval(() => {
+    currentNum += step;
+    if (currentNum >= targetNum) {
+      clearInterval(interval);
+      currentNum = targetNum;
+    }
+    element.textContent = currentNum;
+  }, 50); // update every 50ms
+}
